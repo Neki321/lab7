6b4c8e0860e86fa5d7527ed8a2acef5829bc2f8d
Neki321 <vlad.furman.13@gmail.com>
2023-01-11 14:10:10 +0200

    Initial commit

---
+++ lab7.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int pp(int n)
+{
+    static int Sum = 0;
+    Sum += n;
+    return Sum;
+}
+
+int mm(int n)
+{
+    static int product = 1;
+    product *= n;
+    return product;
+}
+void main()
+    {
+    int n, Sum, product;
+    cout << "Enter a number ";
+    cin >> n;
+    for (int i = 1; i <= n; i++) Sum = pp(i), product = mm(i);
+    cout << "Sum = " << Sum << ", Product = " << product;
+    }
+
